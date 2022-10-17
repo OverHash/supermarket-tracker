ffb7feb9719168a33cdd2a47531a64c820962bac
OverHash <46231745+OverHash@users.noreply.github.com>
2022-10-17 23:10:35 +1300

    Refactor api key retrieval
    Moves the API key retrieval into its own function, for more clarity.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,21 @@ const SITE_URL: &str = "https://www.countdown.co.nz";
 const BASE_URL: &str = "https://www.countdown.co.nz/api/v1/";
 const DEFAULT_USER_AGENT: &str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36";
 
+async fn get_api_key(client: reqwest::Client) -> Result<String, reqwest::Error> {
+    let html_res = client.get(SITE_URL).send().await?.text().await?;
+
+    let start_api_key = html_res
+        .find("apikey=")
+        .expect("Failed to find start of API key")
+        + "apikey=".len();
+    let end_api_key = html_res[start_api_key..]
+        .find('"')
+        .map(|n| n + start_api_key)
+        .expect("Failed to find end of API key");
+
+    Ok(String::from(&html_res[start_api_key..end_api_key]))
+}
+
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
     let client = {
@@ -18,20 +33,8 @@ async fn main() -> Result<(), reqwest::Error> {
             .expect("Failed to create http client")
     };
 
-    let res = client.get(SITE_URL).send().await?.text().await?;
-    let start_api_key = res
-        .find("apikey=")
-        .expect("Failed to find start of API key")
-        + "apikey=".len();
-    let end_api_key = res[start_api_key..]
-        .find('"')
-        .map(|n| n + start_api_key)
-        .expect("Failed to find end of API key");
-    let full_api_key = &res[start_api_key..end_api_key];
-    println!("API Key = {full_api_key:?}");
+    let api_key = get_api_key(client).await?;
+    println!("Retrieved API key: {api_key:?}");
 
-    std::fs::write("./output.html", res).expect("Failed to write output");
-
-    println!("Hello, {BASE_URL}");
     Ok(())
 }
