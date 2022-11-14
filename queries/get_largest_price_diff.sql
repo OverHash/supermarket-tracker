4fbc66464321e4b57522065cb123107c3db86356
OverHash <46231745+OverHash@users.noreply.github.com>
2022-11-14 18:21:12 +1300

    Display name of product in price difference query

--- queries/get_largest_price_diff.sql
+++ queries/get_largest_price_diff.sql
@@ -1,16 +1,20 @@
-with price_history as (
+WITH price_history AS (
 	SELECT
 		product_id,
+		name,
 		SUM(CASE WHEN rank = 1 THEN cost_in_cents ELSE null END) today, 
 		SUM(CASE WHEN rank = 2 THEN cost_in_cents ELSE null END) yesterday,
 		SUM(CASE WHEN rank = 1 THEN cost_in_cents ELSE 0 END) -
 		SUM(CASE WHEN rank = 2 THEN cost_in_cents ELSE 0 END) as diff
-	FROM (SELECT product_id, cost_in_cents, time, rank()
+	FROM (SELECT product_id, cost_in_cents, time, name, rank()
 		OVER (PARTITION BY product_id ORDER BY time DESC) AS rank
-		FROM prices WHERE time BETWEEN current_date - 1 AND current_date + 1
+		FROM prices
+			JOIN products ON products.id = prices.product_id
+			JOIN countdown_products ON countdown_products.id = products.id
+		WHERE time BETWEEN current_date - 1 AND current_date + 1
 	) AS p
 	WHERE rank IN (1,2)
-	GROUP BY product_id
+	GROUP BY (product_id, name)
 )
-SELECT * FROM price_history WHERE (CASE WHEN today IS null OR yesterday IS null then 'NULL' else 'VALID' end) != 'NULL'
+SELECT * FROM price_history WHERE (CASE WHEN today IS null OR yesterday IS null then 'NULL' else 'VALID' end) != 'NULL' AND diff <> 0
 ORDER BY diff DESC;
