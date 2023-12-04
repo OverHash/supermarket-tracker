70aa6c535fc3caf78863803f038a0fd671514952
OverHash <46231745+OverHash@users.noreply.github.com>
2023-12-05 11:46:34 +1300

    Use migrate! macro
    We refactor the docker-compose.yml to not use an external Dockerfile to
    copy the migrations over. This lets sqlx properly apply migrations
    itself when creating the database.

--- src/initialize_database.rs
+++ src/initialize_database.rs
@@ -1,47 +1,8 @@
 use error_stack::Result;
 use sqlx::{Pool, Postgres};
 
-pub async fn initialize_database(conn: &Pool<Postgres>) -> Result<(), sqlx::Error> {
-    // create the supermarket table
-    sqlx::query(
-        r#"CREATE TABLE IF NOT EXISTS countdown_products (
-		id SERIAL PRIMARY KEY,
-		name VARCHAR(255) NOT NULL,
-		barcode VARCHAR(13) NOT NULL,
-		sku VARCHAR(10) NOT NULL UNIQUE
-	)"#,
-    )
-    .execute(conn)
-    .await?;
-
-    sqlx::query(
-        r#"CREATE TABLE IF NOT EXISTS products (
-		id SERIAL PRIMARY KEY,
-		countdown_id INT,
-	
-		CONSTRAINT fk_countdown_product
-			FOREIGN KEY(countdown_id)
-				REFERENCES countdown_products(id)	
-	)"#,
-    )
-    .execute(conn)
-    .await?;
-
-    sqlx::query(
-        r#"CREATE TABLE IF NOT EXISTS prices (
-		id SERIAL PRIMARY KEY,
-		product_id INTEGER NOT NULL,
-		time TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-		cost_in_cents INTEGER NOT NULL,
-		supermarket VARCHAR(255) NOT NULL,
-	
-		CONSTRAINT fk_product
-			FOREIGN KEY(product_id)
-				REFERENCES products(id)
-	)"#,
-    )
-    .execute(conn)
-    .await?;
+pub async fn initialize_database(conn: &Pool<Postgres>) -> Result<(), sqlx::migrate::MigrateError> {
+    sqlx::migrate!().run(conn).await?;
 
     Ok(())
 }
